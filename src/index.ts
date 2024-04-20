12d07cefcb703ba382f386ce96b5ecd26d43d87a
Douglas <douglastelec@gmail.com>
2024-04-20 18:13:48 -0300

    ffff

--- src/index.ts
+++ src/index.ts
@@ -31,4 +31,6 @@ app.post("/test", async (req: Request, res: Response) => {
     await extraAIController.testConnections(req, res)
 })
 
-
+app.get('/healthcheck', (req: Request, res: Response) => {
+    res.status(200).send('Server is running');
+});
