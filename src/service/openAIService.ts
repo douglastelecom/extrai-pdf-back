fe62716657ef59f1135faf35ce9f5685f58c978d
Douglas <douglastelec@gmail.com>
2024-04-21 15:07:45 -0300

    gfgfg

--- src/service/openAIService.ts
+++ src/service/openAIService.ts
@@ -6,7 +6,7 @@ export class OpenAIService {
         const openai = await this.connect(reqBody.openaiKey)
         const responseCompletion = await openai.chat.completions.create({
             messages: [{ role: "system", content: "Você será minha ferramenta para extração de dados." },
-            { role: "user", content: "Extraia as informações em português do artigo abaixo respondendo com um json no formato: " + reqBody.json + "/n " + article }],
+            { role: "user", content: reqBody.instruction + "Extraia as informações em português do artigo abaixo respondendo com um json no formato: " + reqBody.json + "/n " + article }],
             model: reqBody.model,
             response_format: { type: "json_object" }
         });
