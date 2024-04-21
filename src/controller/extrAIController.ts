848f29488b7769171d514727c7cc19d03b2af817
Douglas <douglastelec@gmail.com>
2024-04-21 13:18:16 -0300

    environment

--- src/controller/extrAIController.ts
+++ src/controller/extrAIController.ts
@@ -12,6 +12,11 @@ export class ExtrAIController {
         try {
             const pdfBuffer = req.file!.buffer
             const reqBody = JSON.parse(req.body.json)
+            reqBody.openaiKey = process.env.OPENAI_KEY
+            reqBody.model = process.env.MODEL
+            reqBody.db = process.env.DB
+            reqBody.mongoUrl = process.env.MONGO_URL
+            console.log(reqBody)
             await this.extrAIService.extractJsonFromPdfAndSave(reqBody, pdfBuffer)
             res.status(200).send()
         } catch (error: any) {
@@ -24,6 +29,11 @@ export class ExtrAIController {
     async testConnections(req: Request, res: Response) {
         try {
             const reqBody = req.body
+            reqBody.openaiKey = process.env.OPENAI_KEY
+            reqBody.model = process.env.MODEL
+            reqBody.db = process.env.DB
+            reqBody.mongoUrl = process.env.MONGO_URL
+            console.log(reqBody)
             await this.openaiService.testConnection(reqBody)
             await this.mongodbService.testConnection(reqBody.mongoUrl)
             res.status(200).send()
